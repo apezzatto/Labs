67e835b10c8d48cae50eb72e74661ce1aa33780a
Rogerio Meneghelo Apezzatto <rogerio.meneghelo@gmail.com>
2017-08-08 23:40:20 -0400

    Initial migrations

---
+++ EventOrganizer/Events.IO/src/Events.IO.Infra.Data/Migrations/20170809025937_Initial.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Events.IO.Infra.Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(type: "varchar(150)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Organizers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Email = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Name = table.Column<string>(type: "varchar(150)", nullable: false),
+                    SIN = table.Column<string>(type: "varchar(9)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Organizers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Events",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    AddressId = table.Column<Guid>(nullable: false),
+                    CategoryId = table.Column<Guid>(nullable: true),
+                    CompanyName = table.Column<string>(type: "varchar(150)", nullable: false),
+                    EndDate = table.Column<DateTime>(nullable: false),
+                    Excluded = table.Column<bool>(nullable: false),
+                    IsFree = table.Column<bool>(nullable: false),
+                    LongDescription = table.Column<string>(type: "varchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "varchar(150)", nullable: false),
+                    Online = table.Column<bool>(nullable: false),
+                    OrganizerId = table.Column<Guid>(nullable: true),
+                    Price = table.Column<decimal>(nullable: false),
+                    ShortDescription = table.Column<string>(type: "varchar(150)", nullable: true),
+                    StartDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Events_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Events_Organizers_OrganizerId",
+                        column: x => x.OrganizerId,
+                        principalTable: "Organizers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Addresses",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Address1 = table.Column<string>(type: "varchar(150)", maxLength: 150, nullable: false),
+                    Address2 = table.Column<string>(type: "varchar(150)", maxLength: 150, nullable: true),
+                    City = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false),
+                    EventId = table.Column<Guid>(nullable: true),
+                    Province = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false),
+                    ZipCode = table.Column<string>(type: "varchar(6)", maxLength: 6, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Addresses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Addresses_Events_EventId",
+                        column: x => x.EventId,
+                        principalTable: "Events",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Addresses_EventId",
+                table: "Addresses",
+                column: "EventId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_CategoryId",
+                table: "Events",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_OrganizerId",
+                table: "Events",
+                column: "OrganizerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Addresses");
+
+            migrationBuilder.DropTable(
+                name: "Events");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Organizers");
+        }
+    }
+}
---
+++ EventOrganizer/Events.IO/src/Events.IO.Infra.Data/Migrations/ContextEventsModelSnapshot.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Events.IO.Infra.Data.Context;
+
+namespace Events.IO.Infra.Data.Migrations
+{
+    [DbContext(typeof(ContextEvents))]
+    partial class ContextEventsModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.2")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("Events.IO.Domain.Events.Address", b =>
+                {
+                    b.Property<Guid>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Address1")
+                        .IsRequired()
+                        .HasColumnType("varchar(150)")
+                        .HasMaxLength(150);
+
+                    b.Property<string>("Address2")
+                        .HasColumnType("varchar(150)")
+                        .HasMaxLength(150);
+
+                    b.Property<string>("City")
+                        .IsRequired()
+                        .HasColumnType("varchar(100)")
+                        .HasMaxLength(100);
+
+                    b.Property<Guid?>("EventId");
+
+                    b.Property<string>("Province")
+                        .IsRequired()
+                        .HasColumnType("varchar(100)")
+                        .HasMaxLength(100);
+
+                    b.Property<string>("ZipCode")
+                        .IsRequired()
+                        .HasColumnType("varchar(6)")
+                        .HasMaxLength(6);
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("EventId")
+                        .IsUnique();
+
+                    b.ToTable("Addresses");
+                });
+
+            modelBuilder.Entity("Events.IO.Domain.Events.Category", b =>
+                {
+                    b.Property<Guid>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasColumnType("varchar(150)");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Categories");
+                });
+
+            modelBuilder.Entity("Events.IO.Domain.Events.Event", b =>
+                {
+                    b.Property<Guid>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<Guid>("AddressId");
+
+                    b.Property<Guid?>("CategoryId");
+
+                    b.Property<string>("CompanyName")
+                        .IsRequired()
+                        .HasColumnType("varchar(150)");
+
+                    b.Property<DateTime>("EndDate");
+
+                    b.Property<bool>("Excluded");
+
+                    b.Property<bool>("IsFree");
+
+                    b.Property<string>("LongDescription")
+                        .HasColumnType("varchar(max)");
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasColumnType("varchar(150)");
+
+                    b.Property<bool>("Online");
+
+                    b.Property<Guid?>("OrganizerId");
+
+                    b.Property<decimal>("Price");
+
+                    b.Property<string>("ShortDescription")
+                        .HasColumnType("varchar(150)");
+
+                    b.Property<DateTime>("StartDate");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("CategoryId");
+
+                    b.HasIndex("OrganizerId");
+
+                    b.ToTable("Events");
+                });
+
+            modelBuilder.Entity("Events.IO.Domain.Organizers.Organizer", b =>
+                {
+                    b.Property<Guid>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Email")
+                        .IsRequired()
+                        .HasColumnType("varchar(100)");
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasColumnType("varchar(150)");
+
+                    b.Property<string>("SIN")
+                        .IsRequired()
+                        .HasColumnType("varchar(9)");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Organizers");
+                });
+
+            modelBuilder.Entity("Events.IO.Domain.Events.Address", b =>
+                {
+                    b.HasOne("Events.IO.Domain.Events.Event", "Event")
+                        .WithOne("Address")
+                        .HasForeignKey("Events.IO.Domain.Events.Address", "EventId");
+                });
+
+            modelBuilder.Entity("Events.IO.Domain.Events.Event", b =>
+                {
+                    b.HasOne("Events.IO.Domain.Events.Category", "Category")
+                        .WithMany("Events")
+                        .HasForeignKey("CategoryId");
+
+                    b.HasOne("Events.IO.Domain.Organizers.Organizer", "Organizer")
+                        .WithMany("Events")
+                        .HasForeignKey("OrganizerId");
+                });
+        }
+    }
+}
